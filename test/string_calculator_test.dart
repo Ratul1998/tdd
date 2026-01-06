2fc91d16102660fbd3e33e70ec26e0d0cdb5a494
Ratul <ratulc98@gmail.com>
2026-01-06 08:48:02 +0530

    add Dart unit tests for string calculator

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,8 +1,43 @@
-import 'package:string_calculator/string_calculator.dart';
 import 'package:test/test.dart';
+import 'package:string_calculator/string_calculator.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('String Calculator', () {
+    test('returns 0 for empty string', () {
+      expect(add(''), equals(0));
+    });
+
+    test('returns number for single value', () {
+      expect(add('1'), equals(1));
+    });
+
+    test('adds multiple comma-separated numbers', () {
+      expect(add('1,2,3'), equals(6));
+    });
+
+    test('supports new lines as delimiters', () {
+      expect(add('1\n2,3'), equals(6));
+      expect(add('1\n2\n3'), equals(6));
+    });
+
+    test('throws exception for a negative number', () {
+      expect(
+            () => add('1,-2,3'),
+        throwsA(isA<Exception>()),
+      );
+    });
+
+    test('exception message lists all negative numbers', () {
+      expect(
+            () => add('-1,-2,3'),
+        throwsA(
+          predicate(
+                (e) =>
+            e is Exception &&
+                e.toString().contains('[-1, -2]'),
+          ),
+        ),
+      );
+    });
   });
 }
