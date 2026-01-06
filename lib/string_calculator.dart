c149299d5358335c672f05de1b844cf8a298fd61
Ratul <ratulc98@gmail.com>
2026-01-06 08:47:07 +0530

    throw exception when negative numbers are present

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,8 +1,15 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
 
-  return numbers
+  final values = numbers
       .split(RegExp(r'[,\n]'))
       .map(int.parse)
-      .fold(0, (sum, value) => sum + value);
+      .toList();
+
+  final negatives = values.where((n) => n < 0).toList();
+  if (negatives.isNotEmpty) {
+    throw Exception('Negative numbers not allowed: $negatives');
+  }
+
+  return values.fold(0, (sum, value) => sum + value);
 }
