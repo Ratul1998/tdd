fcd57a3c1f67adabd4417146a5a52e8ab04b7716
Ratul <ratulc98@gmail.com>
2026-01-06 08:46:33 +0530

    allow new lines as valid delimiters

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,7 +2,7 @@ int add(String numbers) {
   if (numbers.isEmpty) return 0;
 
   return numbers
-      .split(',')
+      .split(RegExp(r'[,\n]'))
       .map(int.parse)
       .fold(0, (sum, value) => sum + value);
 }
